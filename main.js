d941e8b1a44e546211fb5a27674066d6e80238ca
anupon27 <anupon.c@ku.th>
2025-12-29 16:53:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+/*
+let firstname = "John doe";
+const idcard = '123';
+console.log('Hello, '+ firstName + "!");
+let age = 30;
+let height = 5.9;
+let isStudent = false;
+
+firstname = 'test';
+console.log('My name is', firstname, 'and I am', age, 'years old.');
+*/
+
+/*
++ บวก
+- ลบ
+* คูณ
+/ หาร
+% หารเอาเศษ mod
+*/
+
+/*
+let number1 = 'test';
+let number2 = 'xxx';
+
+let result = number1 + number2;
+console.log("ผลหาร =", result);
+*/
+
+
+// Conditional
+/*
+let score = prompt("ป้อนคะแนนขอฃคุณ")
+console.log("คะแนนของคุณ " + score)
+*/
+/*
+let Conditional1 = number1 == number2;
+console.log("result of condition is:", Conditional1);
+*/
+// if - else condition
+
+/*
+if (score >= 80) {
+    console.log('A')
+} else if(score >= 70){
+    console.log('B')
+}else if(score >=60){
+    console.log('C')
+}else if(score >= 50){
+    console.log('D')
+}else{
+    console.log('F')
+}*/
+
+/*
+let number1 = 5
+let number2 = 10
+// true && flase
+    let condition = !(number1 >= 3 || number2 >= 5);
+console.log("condition:", condition);
+
+let age = 30;
+let gender = "ชาย";
+
+// 
+if (age >= 20 && gender == "ชาย"){
+    console.log("ผู้ชายที่มีอยุ 20 ปีขึ้นไป");
+}
+*/
+
+/*
+let number = 20
+
+if (number % 2 !=0 ){
+    console.log("เป็นเลขคู่")
+}
+*/
+
+let counter = -5;
+while(counter < 10){
+    console.log('Hello World');
+    counter = counter + 1;
+}
+
+for(let i=0; i<10; i++){
+    console.log('Hello World from for loop');
+}
