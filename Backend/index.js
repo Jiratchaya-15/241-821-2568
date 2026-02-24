8bfa7d2470477e7b255e22795b65e53922003d4a
jiratchaya <Jiratchaya.kaewch@ku.th>
2026-02-24 14:58:58 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,39 @@
-// ทำการ import http module เพื่อสร้าง server
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
+const app = express();
+const port = 8000
+app.use(bodyParser.json());
 
-// กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งาน เว็บผ่านบราวเซอร์ ที่ localhost:8000
-const requestlistener = function(req, res) {
-    res.writeHead(200);
-    res.end('My First Server!');
+let conn = null
+const initDBConnection = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8821
+    })
 }
 
-//run server
-const server = http.createServer(requestlistener);
-server.listen(port, host, () => {
-    console.log('Server is running at http://${host}:${port}');
+//path = GET /users สำหรับ get ข้อมูล user ทั้งหมด
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+
+// path = POST /users สำหรับเพิ่ม user ใหม่
+app.post('/users', async (req, res) => {
+   let user = req.body;
+   const results = await conn.query('INSERT INTO users SET ?', user);
+   console.log('results:', results);
+    res.json({
+        message: 'User created successfully',
+        data: results[0]
+    });
+})
+
+app.listen(port, async () => {
+    await initDBConnection();
+    console.log(`Server is running on port ${port}`)
 });
